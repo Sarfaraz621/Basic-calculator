afe0419748bcd3010528a103238def730514ca59
Sarfaraz621 <103789573+Sarfaraz621@users.noreply.github.com>
2022-06-09 16:52:04 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,40 @@
+let screen=document.getElementById('input');
+const btn=document.querySelectorAll('button');
+let ScreenValue="";
+for(item of btn)
+{
+item.addEventListener('click',(e)=>{
+    btnText=e.target.innerText;
+    //console.log("btn text = ",btnText);
+    if(btnText=='X')
+    {
+        btnText='*';
+        ScreenValue+=btnText;
+        screen.value=ScreenValue;
+    }
+    else if(btnText=='=')
+    {
+        screen.value=eval(ScreenValue);
+    }
+    else if(btnText=='C')
+    {
+        ScreenValue="";
+        screen.value="";
+    }
+    else
+    {
+        ScreenValue+=btnText;
+        if(ScreenValue=='0/0=')
+        {
+            screen.value="NaN";
+        }
+        else if(ScreenValue=='/0=')
+        {
+            screen.value="Infinity";
+        }
+        else
+        {
+            screen.value=ScreenValue;
+        }       
+    }
+})}
